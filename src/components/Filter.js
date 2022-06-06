9797a2ecb2d91ec3b98a198be5fc22154aa5aada
Takashi <th2826@columbia.edu>
2022-06-06 10:24:20 -0400

    Filter component styling updated

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,6 +4,20 @@ function Filter(){
   return (
     <div id="filter">
       <h3>Filter</h3>
+      <ul>
+        <li>
+          <input type="checkbox" defaultChecked/>
+          <i className="arrow"></i>
+          <h4>genre</h4>
+          <p>This UI was written in HTML and CSS.</p>
+        </li>
+        <li>
+          <input type="checkbox" defaultChecked/>
+          <i className="arrow"></i>
+          <h4>technology</h4>
+          <p>Using the sibling and checked selectors</p>
+        </li>
+      </ul>
     </div>
   )
 }
